6bf2ba27359eb7c8fd9cace924c5c0459e8d0d1b
Adrian McDaniel <adriandmcdaniel@gmail.com>
2016-12-01 08:42:38 -0500

    first commit

---
+++ 1130Homework.playground/Pages/Startupizer.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+// creating a strtupzer function of the String type that counts vowels in order
+// and also removes every odd vowel from the returning String
+
+enum Vowels {
+    case a(Int)
+    case e(Int)
+    case i(Int)
+    case o(Int)
+    case u(Int)
+}
+
+
+func strtupzer(n: Int) -> String {
+    
